3e69b19fc30e367da2b6676b9b54d6b7d6516809
stigsanek <stigsanek@yandex.ru>
2021-01-26 13:55:28 +0400

    Добавляет .env и роуты

---
+++ config/.env.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    // Database
+    'db' => [
+        // mysql:host=your_host;dbname=your_db;
+        'dsn' => 'pgsql:host=localhost;dbname=miarengine;',
+        'user' => '',
+        'password' => ''
+    ]
+];
---
+++ config/routes.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'admin' => 'admin/index',
+
+    'logout' => 'user/logout',
+    'profile/info' => 'user/info',
+    'profile/security' => 'user/security',
+
+    'index' => 'main/index',
+    '' => 'main/index',
+];
