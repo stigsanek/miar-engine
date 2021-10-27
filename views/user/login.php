3a65b10e4c920921aeb3349a015bf21ae24eb585
stigsanek <stigsanek@yandex.ru>
2021-10-27 12:26:26 +0400

    Добавляет атрибут id для полей форм

--- views/user/login.php
+++ views/user/login.php
@@ -9,14 +9,14 @@
         <form method="POST">
             <div>
                 <label for="login">Login</label>
-                <input class="<?php if (isset($errors['login'])) : ?>is-invalid<?php endif; ?>" type="text" name="login" placeholder="Login" autocomplete="on" autofocus>
+                <input id="login" class="<?php if (isset($errors['login'])) : ?>is-invalid<?php endif; ?>" type="text" name="login" placeholder="Login" autocomplete="on" autofocus>
                 <?php if (isset($errors['login'])) : ?>
                     <p><?= $errors['login']; ?></p>
                 <?php endif; ?>
             </div>
             <div>
                 <label for="password">Password</label>
-                <input class="<?php if (isset($errors['password'])) : ?>is-invalid<?php endif; ?>" type="password" name="password" placeholder="Password" autocomplete="on">
+                <input id="password" class="<?php if (isset($errors['password'])) : ?>is-invalid<?php endif; ?>" type="password" name="password" placeholder="Password" autocomplete="on">
                 <?php if (isset($errors['password'])) : ?>
                     <p><?= $errors['password']; ?></p>
                 <?php endif; ?>
--- views/user/security.php
+++ views/user/security.php
@@ -4,14 +4,14 @@
         <h3>Change password</h3>
         <div>
             <label for="cur_password">Current password</label>
-            <input class="<?php if (isset($errors['cur_password'])) : ?>is-invalid<?php endif; ?>" type="password" name="cur_password" placeholder="Current password" autocomplete="new-password" autofocus>
+            <input id="cur_password" class="<?php if (isset($errors['cur_password'])) : ?>is-invalid<?php endif; ?>" type="password" name="cur_password" placeholder="Current password" autocomplete="new-password" autofocus>
             <?php if (isset($errors['cur_password'])) : ?>
                 <p><?= $errors['cur_password']; ?></p>
             <?php endif; ?>
         </div>
         <div>
             <label for="new_password">New password</label>
-            <input class="<?php if (isset($errors['new_password'])) : ?>is-invalid<?php endif; ?>" type="password" name="new_password" aria-describedby="new_password_help" placeholder="New password" autocomplete="new-password">
+            <input id="new_password" class="<?php if (isset($errors['new_password'])) : ?>is-invalid<?php endif; ?>" type="password" name="new_password" aria-describedby="new_password_help" placeholder="New password" autocomplete="new-password">
             <?php if (isset($errors['new_password'])) : ?>
                 <p><?= $errors['new_password']; ?></p>
             <?php endif; ?>
@@ -19,7 +19,7 @@
         </div>
         <div>
             <label for="new_password_repeat">Confirm new password</label>
-            <input class="<?php if (isset($errors['new_password_repeat'])) : ?>is-invalid<?php endif; ?>" type="password" name="new_password_repeat" aria-describedby="new_password_repeat_help" placeholder="Confirm new password" autocomplete="new-password">
+            <input id="new_password_repeat" class="<?php if (isset($errors['new_password_repeat'])) : ?>is-invalid<?php endif; ?>" type="password" name="new_password_repeat" aria-describedby="new_password_repeat_help" placeholder="Confirm new password" autocomplete="new-password">
             <?php if (isset($errors['new_password_repeat'])) : ?>
                 <p><?= $errors['new_password_repeat']; ?></p>
             <?php endif; ?>
