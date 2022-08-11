2e092e518ed58b08eb4af1fc680d8732dea4cc93
stigsanek <alexander.e.egorov@pikta.ru>
2022-08-11 17:27:56 +0400

    Добавляет режим отображения ошибок PDO

--- src/Components/Database.php
+++ src/Components/Database.php
@@ -15,7 +15,11 @@ class Database
      */
     public static function getDataBase()
     {
-        $option = [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_EMULATE_PREPARES => false];
+        $option = [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ];
 
         return new PDO($_ENV['DB_DSN'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $option);
     }
