b53367c399ee0728ecabaa9434dfd12bd1315daf
stigsanek <alexander.e.egorov@pikta.ru>
2022-08-11 15:31:27 +0400

    Удаляет файлы конфигурации

--- config/.env.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'debug' => true,
-    'timezone' => 'Europe/Samara'
-];
--- config/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    // mysql:host=your_host;dbname=your_db;
-    'dsn' => 'pgsql:host=localhost;dbname=miarengine;',
-    'user' => '',
-    'password' => ''
-];
